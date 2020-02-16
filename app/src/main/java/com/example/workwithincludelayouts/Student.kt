31afd4369d9cfc0fc4c4e7349184e9f95f392c5e
unknown <chageshii@gmail.com>
2020-02-16 21:48:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/workwithincludelayouts/Student.kt
@@ -0,0 +1,14 @@
+package com.example.workwithincludelayouts
+
+class Student {
+    constructor(studentName: String?, studentEmail: String?) {
+        this.studentName = studentName
+        this.studentEmail = studentEmail
+    }
+
+    constructor() {}
+
+    var studentName: String? = null
+    var studentEmail: String? = null
+
+}
